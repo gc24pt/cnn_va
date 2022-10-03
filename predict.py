33087bfc5d8be7496bb7de79ee15a0940f928095
gc24pt <goncalocabaco@protonmail.com>
2022-10-03 20:24:28 +0100

    Saved model

--- predict.py
+++ predict.py
@@ -4,17 +4,17 @@ import numpy as np
 from natsort import natsorted
 from tensorflow.keras.models import load_model
 
-def load(path1, path2):
+def load(path):
     images = []
-    for file in natsorted(glob.glob(path1)):
+    for file in natsorted(glob.glob(path)):
         image = cv2.imread(file)
         images.append(image)
     return np.array(images)
 
 def predict(path):
     data = load(path + 'specs/*.png')
-    model = load_model('checkpoint/')
+    model = load_model('model/')
     ypred = model.predict(data)
     return ypred
 
-#ypred = predict('Dados16_200s/')
+#ypred = predict('test/')
